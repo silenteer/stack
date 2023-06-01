720d96c578daa1d858ff68102360581856649fe1
Duke <duc@silentium.io>
2023-06-01 10:30:25 +0000

    feat: added tailwind and shared components to frontend app

---
+++ frontend/app/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@frontend/components/button"
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-red-500">Hello world!</h1>
+      <Button variant={"outline"}>Test</Button>
+    </>
+  );
+}
