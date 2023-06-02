4d2586a432fbddc6e98e4f1d0d4d3d2339a6eae7
Duke <duc@silentium.io>
2023-06-02 11:05:38 +0000

    feat: added sample ui

--- frontend/app/pages/index.tsx
+++ frontend/app/pages/index.tsx
@@ -1,10 +1,25 @@
-import { Button } from "@frontend/components/button"
+import { Button } from "@frontend/ui/button"
+
+import { Terminal, Waves } from "lucide-react"
+ 
+import { Alert, AlertDescription, AlertTitle } from "@frontend/ui/alert"
+ 
+function AlertDemo() {
+  return (
+    <Alert>
+      <Terminal className="h-4 w-4" />
+      <AlertTitle>Heads up!</AlertTitle>
+      <AlertDescription>
+        You can add components to your app using the cli.
+      </AlertDescription>
+    </Alert>
+  )
+}
 
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-red-500">Hello world!</h1>
-      <Button variant={"outline"}>Test</Button>
+      <AlertDemo />
     </>
   );
 }
---
+++ frontend/ui/index.ts
@@ -0,0 +1,1 @@
+// Don't need to add anything here. This is an index so the monorepo would work as expected
