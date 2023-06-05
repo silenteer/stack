14bab8de5acfe51cf127e84141815461eb6f25a3
Duke <duc@silentium.io>
2023-06-05 10:37:48 +0000

    feat: added storybook to ui

---
+++ frontend/ui/.storybook/main.js
@@ -0,0 +1,20 @@
+/** @type { import('@storybook/react-vite').StorybookConfig } */
+const config = {
+  stories: [
+    "../**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    '@storybook/addon-styling'
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+};
+export default config;
---
+++ frontend/ui/button/button.stories.tsx
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Button } from '.';
+
+const meta: Meta<typeof Button> = {
+  /* 👇 The title prop is optional.
+   * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+   * to learn how to generate automatic titles
+   */
+  title: 'Button',
+  component: Button,
+};
+
+export default meta;
+type Story = StoryObj<typeof Button>;
+
+export const Primary: Story = {
+  render() {
+    return <Button>Hello</Button>
+  },
+  parameters: {
+    controls: { hideNoControlsWarning: true },
+  },
+};
